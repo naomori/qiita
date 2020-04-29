9bff111d5f2e7ff16420aae2c5a345a312b1da33
naomori <naoki.morita@gmail.com>
2020-04-29 19:17:38 +0900

    update

--- deploy_test.py
+++ deploy_test.py
@@ -1,7 +1,7 @@
 from qiita_v2.client import QiitaClient
 
 config_file = "config.yaml"
-user_name = "<user_name>"
+user_name = "naomori"
 client = QiitaClient(config_file=config_file)
 response = client.get_user(user_name)
 print(response.to_json())
